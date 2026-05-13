ab4300a848f479b19e6d7aa4ad7a05b960b5ceae
cxayg <3047868046+zgyzgd1@users.noreply.github.com>
2026-05-13 15:29:40 +0800

    Fix build-blocking dependency versions and Kotlin configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("com.android.application") version "9.1.1" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
-    id("com.google.devtools.ksp") version "2.3.6" apply false
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
 }
